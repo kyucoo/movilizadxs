163556a8c5ed64689ea9f4f0a550dc69859157e9
Juan Jara <53664529+Jez-design@users.noreply.github.com>
2023-06-06 18:25:23 -0400

    Cambio estructura html footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,25 @@
+<footer class="footer" id="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 p-0">
+                <div class="wrapp">
+                    <div class="wrapp__titleBrand">
+                        <h1 class="wrapp__titleBrand--h1">
+                            <img class="h1-img" src="" alt="Logotipo Movilizados">
+                        </h1>                        
+                    </div>
+                    <div class="wrapp__inforDiv">
+                        <ul class="wrapp__inforDiv--ul">
+                            <li class="ul-li"><p>Instagram: @moviliza2.porla.dignidad</p></li>
+                            <li class="ul-li"><p>+ 10.000 propiedades vendidas.</p></li>
+                            <li class="ul-li"><p>Facebook: h􀆩ps://www.facebook.com/moviliza2porladignidad</p></li>
+                            <li class="ul-li"><p>Email: moviliza2.porla.dignidad@gmail.com</p></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <!-- web design & desarrollo realizado por Movilizadxs Por La Dignidad -->
 </footer>
 </body>
